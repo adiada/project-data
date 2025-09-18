5584c4bf2faad399ec46cd4d673b9e985a524ed3
adharsh.suresh <adharshsuresh1997@gmail.com>
2025-09-18 10:12:26 +0530

    chore: added cors support

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const jsonServer = require("json-server"); // importing json-server library
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+server.use(cors({ origin: "*" }));
+
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
